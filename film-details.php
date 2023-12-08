882443d466d1e3fd1cf2ed63787dc583f60d9446
c3052701 <c3052701@hallam.shu.ac.uk>
2023-12-08 10:51:13 +0000

    title

--- film-details.php
+++ film-details.php
@@ -14,7 +14,7 @@ $obj = $result->fetch_object();
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Film Titl Here</title>
+<title>Film Title Here</title>
 <link rel="stylesheet" href="css/mobile.css" />
 <link
       rel="stylesheet"
